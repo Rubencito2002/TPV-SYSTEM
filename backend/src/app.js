b359a5226437543a323a3d863b2cda0cd4affed7
rubencito2002 <ruben.machado1920@gmail.com>
2025-07-22 18:25:17 +0200

    feat: agregar manejo de errores para rutas no encontradas

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,11 @@ app.use(express.json());
 
 app.use('/api/productos', productosRouter);
 
+// Mensaje de error para rutas no encontradas
+app.use((req, res) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
